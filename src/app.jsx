9a8717e63f5175af67fc413fed8b04d78e546f9c
Muhammad Raihan Suryanom <raihansuryanom@gmail.com>
2022-05-23 15:51:48 +0700

    create search feature

--- src/app.jsx
+++ src/app.jsx
@@ -1,18 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FormInput, Header, SectionNotes } from "./components";
 import style from "./styles/app.module.css";
 import { getInitialData } from "./utils";
 
 function App() {
   const [query, setQuery] = useState("");
+  const [searchNotes, setSearchNotes] = useState([]);
   const [notes, setNotes] = useState(getInitialData());
 
-  const activeNotes = notes.filter((note) => !note.archived);
-  const archivedNotes = notes.filter((note) => note.archived);
+  const activeNotes = (searchNotes || notes).filter((note) => !note.archived);
+  const archivedNotes = (searchNotes || notes).filter((note) => note.archived);
+
+  useEffect(() => {
+    setSearchNotes(notes.filter((note) => note.title.toLowerCase().includes(query.toLowerCase())));
+  }, [query, notes]);
 
   return (
     <>
-      <Header search={query} updateQuery={setQuery} />
+      <Header search={query} updateQuery={setQuery} updateNotes={setNotes} />
       <main className={style.main}>
         <FormInput updateNotes={setNotes} />
         <SectionNotes label='Active Notes' notes={activeNotes} setNotes={setNotes} />
